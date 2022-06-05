1fba57b4723c90dec923979e0536f57d7cc50f3f
amarahmednet <amarahmednet@gmail.com>
2022-06-05 12:00:36 +0300

    all

---
+++ food/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+
+class item(models.Model):
+
+    def __str__(self):
+        return self.item_name
+
+    item_name = models.CharField(max_length=200)
+    item_discription = models.CharField(max_length=200)
+    item_number = models.IntegerField()
+
+
+
---
+++ food/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [ 
+    path('', views.index, name='index'),
+    path('item/', views.item, name='item'),
+ 
+]
+
