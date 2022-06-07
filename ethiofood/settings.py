580413308f71a7d21c4875dacfe3a351d28a79c2
amarahmednet <amarahmednet@gmail.com>
2022-06-07 16:03:57 +0300

    nothing much

--- ethiofood/settings.py
+++ ethiofood/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ethiofood.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\NJU\Documents\python\django\ethiofood\food\template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
