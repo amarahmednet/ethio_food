8c19aab19e41c3e3c4b46e4673abe80a0327e72d
amarahmednet <amarahmednet@gmail.com>
2022-06-11 21:23:08 +0300

    h

--- food/views.py
+++ food/views.py
@@ -4,20 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    # index_template = loader.get_template('food/index.html')
     items_ = item.objects.all()
     context = {
-    #context
     'items_':items_,
     }
     return render(request, 'food/index.html', context)
 
 def detail(request, item_id):
-
     items = item.objects.get(pk = item_id)
-
     context = {
         'items':items
     }
-    
     return render(request, 'food/detail.html', context)
